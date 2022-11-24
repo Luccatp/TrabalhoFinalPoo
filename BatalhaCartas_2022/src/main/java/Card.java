c8274baac2211767b8a6d5e975a8437bb845b10f
Lucca Paradeda <luccatisserparadeda@gmail.com>
2022-11-24 01:04:11 -0300

    feat: IMPLEMENTATION

--- BatalhaCartas_2022/src/main/java/Card.java
+++ BatalhaCartas_2022/src/main/java/Card.java
@@ -1,41 +1,54 @@
 import java.util.*;
 
-public class Card extends Observable{
-    private String id;
-    private String imageId;
-    private int value;
-    private boolean faceUp;
-    
-    public Card(String anId,String anImageId,int val){
-        id = anId;
-        imageId = anImageId;
-        value = val;
-        faceUp = true;
-    }
-    
-    public String getId(){
-        return(id);
-    }
-    
-    public String getImageId(){
-        return(imageId);
-    }
-    
-    public int getValue(){
-        return(value);
-    }
-    
-    public boolean isFacedUp(){
-        return(faceUp);
-    }
-    
-    public void flip(){
-        if (faceUp == true){
-            faceUp = false;
-        }else{
-            faceUp = true;
-        }
-        setChanged();
-        notifyObservers();
-    }
+enum Type {
+	FIRE,
+	WATER,
+	PLANT,
+
+}
+
+public class Card extends Observable {
+	private String id;
+	private String imageId;
+	private int value;
+	private Type type;
+	private boolean faceUp;
+
+	public Card(String anId, String anImageId, int val, Type type) {
+		id = anId;
+		imageId = anImageId;
+		value = val;
+		this.type = type;
+		faceUp = true;
+	}
+
+	public String getId() {
+		return (id);
+	}
+
+	public String getImageId() {
+		return (imageId);
+	}
+
+	public int getValue() {
+		return (value);
+	}
+
+	public Type getType() {
+		return (type);
+	}
+
+	public boolean isFacedUp() {
+		return (faceUp);
+	}
+
+	public void flip() {
+		if (faceUp == true) {
+			faceUp = false;
+		} else {
+			faceUp = true;
+		}
+		setChanged();
+		notifyObservers();
+	}
 }
--- BatalhaCartas_2022/src/main/java/CardDeck.java
+++ BatalhaCartas_2022/src/main/java/CardDeck.java
@@ -1,47 +1,62 @@
 import java.util.*;
 
 // Esta classe tem de ser um container de cartas observavel ...
-public class CardDeck extends Observable{
-    public static final int NCARDS = 6;
-    private List<Card> cartas;
-    private Card selected;
-    
-    public CardDeck(){
-       cartas = new ArrayList<Card>(NCARDS);
-       selected = null;
-       Random r = new Random();
-       for(int i=0;i<NCARDS;i++){
-           int n = r.nextInt(10)+1;
-           Card c = new Card("C"+n,"img"+n,n);
-           c.flip();
-           cartas.add(c);
-       }
-    }
-        
-    public List<Card> getCards(){
-        return(cartas);
-    }
+public class CardDeck extends Observable {
+	public static final int NCARDS = 6;
+	private List<Card> cartas;
+	private Card selected;
 
-    public int getNumberOfCards(){
-        return(cartas.size());
-    }
-    
-    public void removeSel(){
-        if (selected == null){
-            return;
-        }
-        cartas.remove(selected);
-        selected = null;
-        GameEvent gameEvent = new GameEvent(GameEvent.Target.DECK,GameEvent.Action.REMOVESEL,"");
-        setChanged();
-        notifyObservers(gameEvent);
-    }    
-        
-    public void setSelectedCard(Card card){
-        selected = card;
-    }
-    
-    public Card getSelectedCard(){
-        return(selected);
-    }
+	public CardDeck() {
+		cartas = new ArrayList<Card>(NCARDS);
+		selected = null;
+		Random r = new Random();
+		Type type;
+		for (int i = 0; i < NCARDS; i++) {
+			int n = r.nextInt(10) + 1;
+			int randomType = r.nextInt(3) + 1;
+			if (randomType == 1) {
+				type = Type.FIRE;
+			} else if (randomType == 2) {
+				type = Type.PLANT;
+			} else if (randomType == 3) {
+				type = Type.WATER;
+			} else if (randomType == 4) {
+				type = Type.PLANT;
+			} else if (randomType == 5) {
+				type = Type.PLANT;
+			} else {
+				type = Type.PLANT;
+			}
+			Card c = new Card("C" + n, "img" + n, n, type);
+			c.flip();
+			cartas.add(c);
+		}
+	}
+
+	public List<Card> getCards() {
+		return (cartas);
+	}
+
+	public int getNumberOfCards() {
+		return (cartas.size());
+	}
+
+	public void removeSel() {
+		if (selected == null) {
+			return;
+		}
+		cartas.remove(selected);
+		selected = null;
+		GameEvent gameEvent = new GameEvent(GameEvent.Target.DECK, GameEvent.Action.REMOVESEL, "");
+		setChanged();
+		notifyObservers(gameEvent);
+	}
+
+	public void setSelectedCard(Card card) {
+		selected = card;
+	}
+
+	public Card getSelectedCard() {
+		return (selected);
+	}
 }
